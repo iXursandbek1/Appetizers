87ca0e11b7c8d84d2e33cd243e3ac4a6e0e9f01c
Xursandbek <xursandbek06082001@mail.com>
2023-09-20 11:11:59 +0500

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Xursandbek Qambaraliyev on 20/09/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
